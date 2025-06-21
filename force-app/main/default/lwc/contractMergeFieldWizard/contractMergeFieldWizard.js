2d46185b77c3fe8e9b77db57901bc370e7967252
Stefan Serkin <stefanserkin@gmail.com>
2025-06-21 12:02:16 -0400

    Scaffolding for declarative merge field insertion

--- force-app/main/default/lwc/contractMergeFieldWizard/contractMergeFieldWizard.js
+++ force-app/main/default/lwc/contractMergeFieldWizard/contractMergeFieldWizard.js
@@ -12,30 +12,104 @@
  * Asphalt Green Data and Information Systems
  ***********************************************************************/
 import { LightningElement, api, track } from 'lwc';
+import getFieldDescriptors from '@salesforce/apex/ContractTemplateEditorController.getFieldDescriptors';
 
 export default class ContractMergeFieldWizard extends LightningElement {
     @api objectApiName;
-    @track selectedField = '';
-    @track tableAlias = '';
+    @track fieldOptions = [];
+    @track breadcrumbTrail = [];
+    @track currentObject = '';
+    @track currentPath = '';
+    @track selectedValue = '';
 
-    @track fieldOptions = [
-        { label: 'Name', value: '{!Name}' },
-        { label: 'Membership Name', value: '{!TREX1__Membership__r.Name}' },
-        { label: 'Contract Item Table', value: '{!tableStart:contractItems}{!Name} - {!Price__c}{!tableEnd}' }
-    ];
-
-    handleFieldChange(event) {
-        this.selectedField = event.detail.value;
+    connectedCallback() {
+        this.loadFieldOptions(this.objectApiName);
     }
 
-    handleAliasChange(event) {
-        this.tableAlias = event.detail.value;
+    loadFieldOptions(objectApiName, relationshipPath = null) {
+        this.currentObject = objectApiName;
+        this.currentPath = relationshipPath || '';
+
+        getFieldDescriptors({ objectApiName, relationshipPath })
+            .then((data) => {
+                this.fieldOptions = data.map(field => {
+                    if (field.apiName.startsWith('table:')) {
+                        const alias = field.apiName.split(':')[1];
+                        return {
+                            label: field.label,
+                            value: `{!tableStart:${alias}}{!Name} - {!Price__c}{!tableEnd}`
+                        };
+                    } else if (field.isRelationship) {
+                        return {
+                            label: field.label + ' (→)',
+                            value: field.apiName,
+                            isRelationship: true,
+                            relationshipName: field.relationshipName,
+                            baseObject: this.currentObject,
+                            fullPath: field.apiName
+                        };
+                    } else {
+                        return {
+                            label: field.label,
+                            value: `{!${field.apiName}}`
+                        };
+                    }
+                });
+            })
+            .catch((error) => {
+                console.error('Error loading fields:', error);
+            });
+    }
+
+    handleFieldSelection(event) {
+        const selected = this.fieldOptions.find(opt => opt.value === event.detail.value);
+        console.log('selected --> ', JSON.stringify(selected));
+        if (selected?.isRelationship) {
+            this.breadcrumbTrail.push({
+                objectApiName: this.currentObject,
+                relationshipPath: this.currentPath,
+                label: selected.label
+            });
+            console.log('breadcrumbTrail --> ', JSON.stringify(this.breadcrumbTrail));
+            const nextPath = selected.fullPath;
+            console.log('nextPath --> ', nextPath);
+            const nextObject = selected.relationshipName;
+            console.log('nextObject --> ', nextObject);
+            this.loadFieldOptions(nextObject, nextPath);
+        } else {
+            this.dispatchEvent(new CustomEvent('mergefieldselected', {
+                detail: { mergeText: selected.value }
+            }));
+        }
+    }
+
+    handleBack() {
+        if (this.breadcrumbTrail.length > 0) {
+            const previous = this.breadcrumbTrail.pop();
+            this.loadFieldOptions(previous.objectApiName, previous.relationshipPath);
+        }
+    }
+
+    handleComboChange(event) {
+        this.selectedValue = event.detail.value;
+        this.handleFieldSelection({ detail: { value: this.selectedValue } });
     }
 
     handleInsert() {
-        let mergeText = this.selectedField;
-        this.dispatchEvent(new CustomEvent('mergefieldselected', {
-            detail: { mergeText }
-        }));
+        const selected = this.fieldOptions.find(opt => opt.value === this.selectedValue);
+        if (selected && !selected.isRelationship) {
+            this.dispatchEvent(new CustomEvent('mergefieldselected', {
+                detail: { mergeText: selected.value }
+            }));
+        }
     }
+
+    get breadcrumbLabels() {
+        return this.breadcrumbTrail.map(crumb => crumb.label);
+    }
+
+    get isAtRoot() {
+        return this.breadcrumbTrail.length === 0;
+    }
+
 }
